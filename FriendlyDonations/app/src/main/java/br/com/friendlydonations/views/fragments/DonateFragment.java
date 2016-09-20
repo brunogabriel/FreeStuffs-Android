eece8ece4a4cc2f9f2d7d2994c73ab254cbb18ce
brunogabriel <bruno.gsantos89@gmail.com>
2016-09-19 23:46:05 -0300

    Added structure on forms

--- FriendlyDonations/app/src/main/java/br/com/friendlydonations/views/fragments/DonateFragment.java
+++ FriendlyDonations/app/src/main/java/br/com/friendlydonations/views/fragments/DonateFragment.java
@@ -3,30 +3,47 @@ package br.com.friendlydonations.views.fragments;
 /**
  * Created by brunogabriel on 9/19/16.
  */
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.renderscript.Type;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import br.com.friendlydonations.R;
 import br.com.friendlydonations.managers.BaseFragment;
+import br.com.friendlydonations.utils.TypefaceMaker;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by brunogabriel on 8/27/16.
  */
-public class DonateFragment extends BaseFragment {
+public class DonateFragment extends BaseFragment implements View.OnFocusChangeListener{
 
     // Views
     protected View rootView;
+
+    // Typefaces
+    Typeface mRobotoLight;
+    Typeface mMonserratRegular;
+
+    @BindView(R.id.etProductItemTitle) AppCompatEditText etProductItemTitle;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // mainActivity = (MainActivity) getActivity();
         rootView = inflater.inflate(R.layout.fragment_donate, container, false);
+        initTypefaces();
         ButterKnife.bind(this, rootView);
         //setHasOptionsMenu(true);
         initUI();
@@ -34,7 +51,14 @@ public class DonateFragment extends BaseFragment {
     }
 
     @Override
-    public void initUI() {}
+    public void initUI() {
+        etProductItemTitle.setOnFocusChangeListener(this);
+    }
+
+    public void initTypefaces() {
+        mMonserratRegular = TypefaceMaker.createTypeFace(getActivity(), TypefaceMaker.FontFamily.MontserratRegular);
+        mRobotoLight = TypefaceMaker.createTypeFace(getActivity(), TypefaceMaker.FontFamily.RobotoLight);
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -53,5 +77,17 @@ public class DonateFragment extends BaseFragment {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+
+        if (view instanceof AppCompatEditText) {
+            if (hasFocus) {
+                setTypeface(mMonserratRegular, (TextView)view);
+            } else {
+                setTypeface(mRobotoLight, (TextView)view);
+            }
+        }
     }
 }
