9122ded19ab965025ca00807e920c7c04da19027
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2017-04-24 23:08:54 -0300

    Added View in Activities

--- FriendlyDonations/app/src/main/java/br/com/friendlydonations/application/main/MainActivity.java
+++ FriendlyDonations/app/src/main/java/br/com/friendlydonations/application/main/MainActivity.java
@@ -10,7 +10,7 @@ import br.com.friendlydonations.shared.BaseActivity;
  * Created by brunogabriel on 24/04/17.
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements MainView {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
