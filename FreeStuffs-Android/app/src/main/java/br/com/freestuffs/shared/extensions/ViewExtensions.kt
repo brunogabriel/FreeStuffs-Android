607961564a84c964478751beb2cd5852e141336e
Bruno Santos <bruno.gsantos89@gmail.com>
2017-09-16 14:07:13 -0300

    Added butterknife kotlin version

--- FreeStuffs-Android/app/src/main/java/br/com/freestuffs/shared/extensions/ViewExtensions.kt
+++ FreeStuffs-Android/app/src/main/java/br/com/freestuffs/shared/extensions/ViewExtensions.kt
@@ -1,10 +1,6 @@
 package br.com.freestuffs.shared.extensions
 
-import android.app.Activity
-import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
-import android.support.v4.app.Fragment
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,12 +10,6 @@ import com.bumptech.glide.Glide
 /**
  * Created by bruno on 24/08/2017.
  */
-
-// Binding
-private fun <T> unsafeLazy(initializer: () -> T) = lazy(LazyThreadSafetyMode.NONE, initializer)
-fun <T: View> Fragment.bind(@IdRes idResource: Int): Lazy<T> = unsafeLazy { view?.findViewById<T>(idResource) as T}
-fun <T: View> Activity.bind(@IdRes idResource: Int): Lazy<T> = unsafeLazy { findViewById<T>(idResource) as T}
-fun <T: View> RecyclerView.ViewHolder.bind(@IdRes idResource: Int): Lazy<T> = unsafeLazy { itemView?.findViewById<T>(idResource) as T}
 
 // Layout Inflater
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
